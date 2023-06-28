910bbade9b2a2f4eefaa631a659f86591ab98455
Peter Daniel <peterdaniel@peters-macbook-air.local>
2023-06-28 18:43:36 +0100

    new variables

--- variables.js
+++ variables.js
@@ -3,12 +3,20 @@ export function updateVariables() {
 	
 	variables.push(
 		{
+			name: 'Workspace',
+			variableId: 'workspace',
+		},
+		{
 			name: 'Current Timer Id',
 			variableId: 'timerId',
 		},
 		{
 			name: 'Current Timer Duration',
 			variableId: 'timerDuration',
+		},
+		{
+			name: 'Last Timer Duration',
+			variableId: 'lastTimerDuration',
 		},
 		{
 			name: 'Current Timer Description',
