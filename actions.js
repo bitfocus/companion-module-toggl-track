3f50c7e71afc0b9ec7ec13b9eeba2aef8a6869ef
Peter Daniel <peterdaniel@peters-macbook-air.local>
2023-06-29 12:17:31 +0100

    beta v2

---
+++ actions.js
@@ -0,0 +1,51 @@
+export function updateActions() {
+	let actions = {}
+
+	actions['startNewTimer'] = {
+		name: 'Start New Timer',
+		options: [
+			{
+				type: 'textinput',
+				label: 'Description',
+				id: 'description',
+				default: '',
+			},
+			{
+				type: 'dropdown',
+				label: 'Project',
+				id: 'project',
+				default: '0',
+				choices: this.projects,
+			},
+		],
+		callback: ({ options }) => {
+			this.startTimer(options.project, options.description)
+		},
+	}
+	
+	actions['getCurrentTimer'] = {
+		name: 'Get Current Timer',
+		options: [],
+		callback: (action) => {
+			this.getCurrentTimer()
+		},
+	}
+	
+	actions['stopCurrentTimer'] = {
+		name: 'Stop Current Timer',
+		options: [],
+		callback: (action) => {
+			this.stopTimer()
+		},
+	}
+	
+	actions['refreshProjects'] = {
+		name: 'Refresh Project List',
+		options: [],
+		callback: (action) => {
+			this.getWorkspace()
+		},
+	}
+
+	this.setActionDefinitions(actions)
+}
